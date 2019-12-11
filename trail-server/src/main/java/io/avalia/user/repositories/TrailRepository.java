4a874dfb4812c114834dbe96cd7e7a3f56bd6775
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-12-11 17:38:09 +0100

    base for user, trail, registration api

--- trail-server/src/main/java/io/avalia/user/repositories/TrailRepository.java
+++
@@ -1,7 +0,0 @@
-package io.avalia.user.repositories;
-
-import io.avalia.user.entities.TrailEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TrailRepository  extends CrudRepository<TrailEntity, Long> {
-}
